aa417d5e40d49265b1f58962d6402a6c78679da4
Josh Archer <jarcher@greenriver.edu>
2019-05-08 11:00:35 -0700

    Added some initial methods to our directed graph.

--- graphs/src/representations/DirectedAL.java
+++ graphs/src/representations/DirectedAL.java
@@ -15,34 +15,87 @@ public class DirectedAL<V>
         adjacencyLists = new HashMap<>();
     }
 
-    public void addVertex(V vertex)
+    //pass any number of vertices into the graph
+    public void addVertex(V... vertices)
     {
-
+        for (V vertex : vertices)
+        {
+            addVertex(vertex);
+        }
     }
 
-    public void addEdge(V from, V to)
+    public boolean addVertex(V vertex)
     {
+        if (containsVertex(vertex))
+        {
+            return false;
+        }
+        else
+        {
+            adjacencyLists.put(vertex, null);
+            return true;
+        }
+    }
 
+    public void addEdge(Edge<V>... edges)
+    {
+        for (Edge<V> edge : edges)
+        {
+            addEdge(edge.from, edge.to);
+        }
+    }
+
+    public boolean addEdge(V from, V to)
+    {
+        //make sure the vertices exist, make sure this is not a duplicate edge
+        if (!adjacencyLists.containsKey(from) || !adjacencyLists.containsKey(to) ||
+            containsEdge(from, to))
+        {
+            return false;
+        }
+
+        if (adjacencyLists.get(from) == null)
+        {
+            adjacencyLists.put(from, new Node<V>(to));
+            return true;
+        }
+        else
+        {
+            //create a new head of the linked list of adjacent vertices
+            Node<V> newNode = new Node<>(to);
+            newNode.next = adjacencyLists.get(from);
+
+            adjacencyLists.put(from, newNode);
+            return true;
+        }
     }
 
     public boolean containsVertex(V vertex)
     {
-        return false;
+        return adjacencyLists.containsKey(vertex);
     }
 
     public boolean containsEdge(V from, V to)
     {
-        return false;
-    }
+        if (!adjacencyLists.containsKey(from) || !adjacencyLists.containsKey(to))
+        {
+            return false;
+        }
 
-    public boolean removeVertex(V vertex)
-    {
-        return false;
-    }
+        Node<V> current = adjacencyLists.get(from);
+        while (current != null)
+        {
+            //is this the "to" vertex
+            if (current.vertex.equals(to))
+            {
+                return true;
+            }
 
-    public boolean removeEdge(V from, V to)
-    {
-        return false;
+            //otherwise move to the next adjacent vertex
+            current = current.next;
+        }
+
+        return false; //we never found the vertex
     }
 
     public void clear()
@@ -65,12 +118,22 @@ public class DirectedAL<V>
         return null;
     }
 
-    public Set<Edge> edges()
+    public Set<Edge<V>> edges()
     {
         return null;
     }
 
-    public class Edge
+    public boolean removeVertex(V vertex)
+    {
+        return false;
+    }
+
+    public boolean removeEdge(V from, V to)
+    {
+        return false;
+    }
+
+    public static class Edge<V>
     {
         private V from;
         private V to;
@@ -114,6 +177,11 @@ public class DirectedAL<V>
         private V vertex;
         private Node next;
         private double weight;
+
+        public Node(V vertex)
+        {
+            this(vertex, null, 0.0);
+        }
 
         public Node(V vertex, Node next, double weight)
         {
--- graphs/src/representations/TestGraph.java
+++ graphs/src/representations/TestGraph.java
@@ -1,10 +1,37 @@
 package representations;
 
+import representations.DirectedAL.Edge;
+
 public class TestGraph
 {
     public static void main(String[] args)
     {
         DirectedAL<Integer> numberGraph = new DirectedAL<>();
-        System.out.println(numberGraph);
+
+        //add a few vertices
+        numberGraph.addVertex(10, 4, 2, 6, 7, 8);
+
+        //add a few edges
+        numberGraph.addEdge(
+            new Edge<>(10, 2, 0),
+            new Edge<>(2, 4, 0),
+            new Edge<>(6, 7, 0),
+            new Edge<>(6, 2, 0)
+        );
+
+        //verify that all is well!
+        System.out.println("Verifying things in the graph: ");
+        System.out.println(numberGraph.containsVertex(10));
+        System.out.println(numberGraph.containsVertex(2));
+        System.out.println(numberGraph.containsVertex(6));
+
+        System.out.println(numberGraph.containsEdge(10, 2));
+        System.out.println(numberGraph.containsEdge(6, 2));
+        System.out.println(numberGraph.containsEdge(6, 7));
+
+        System.out.println("Verifying things not in the graph");
+        System.out.println(numberGraph.containsVertex(11));
+        System.out.println(numberGraph.containsEdge(1, 2)); //missing vertex
+        System.out.println(numberGraph.containsEdge(10, 6)); //missing edge
     }
 }
